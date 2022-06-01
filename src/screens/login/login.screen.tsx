5a042b954c03b406fa68570fb44b79a82214bf94
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-06-01 11:10:23 -0300

    feat: styled login page

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,12 +1,15 @@
 import { Grid } from "@mui/material";
-import React, { useCallback, useEffect, useState } from "react"
-import { Wrapper } from "./login.styled"
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, Error, Input, Wrapper } from "./login.styled";
+import * as yup from 'yup';
 
 const Login = () => {
   const [data, setData] = useState({
     email: '',
     password: ''
   })
+
+  const [error, setError] = useState('')
 
   const handleChange = useCallback(
     ({target}: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,15 +21,44 @@ const Login = () => {
     [setData]
   )
 
-  useEffect(
-    () => console.log(data), [data]
+  const handleSend = useCallback(
+    async () => {
+      try {
+        const schema = yup.object().shape({
+          email: yup.string().required().email(),
+          password: yup.string().required()
+        })
+
+        await schema.validate(data)
+        setError('')
+      } catch (error: any) {
+        setError(error.errors[0])
+      }
+    },
+    [data]
   )
+
+  // useEffect(
+  //   () => console.log(data), [data]
+  // )
 
   return (
     <Wrapper container justifyContent='center' alignContent='center'>
-      <Grid container justifyContent='center' alignContent='center' xs={4}>
-        <input type='email' name='email' onChange={handleChange}/>
-        <input type='password' name='password' onChange={handleChange}/>
+      <Grid container justifyContent='center' alignContent='center' xs={2}>
+        <Input
+         type='email' 
+         name='email' 
+         placeholder='E-mail'
+         onChange={handleChange}
+        />
+        <Input
+         type='password' 
+         name='password' 
+         placeholder='Senha'
+         onChange={handleChange}
+        />
+        <Button onClick={handleSend}>Entrar</Button>
+        <Error>{error}</Error>
       </Grid>
     </Wrapper>
   )
